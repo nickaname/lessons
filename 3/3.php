931fdfc785493e840e38f2cf8d2583af61bc2912
t.kharchenko <frosya.forewer@mail.ru>
2019-04-01 19:10:06 +0300

    last in 1sttripple

--- 3/3.php
+++ 3/3.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 01.04.19
- * Time: 18:10
- */
+
+$str="Привет мир! Я лучший текст из всех, что ты когда-либо видел!";
+//$find=[];
+//$find[]="лучший";
+//$find[]="Привет";
+//$find[]="code";
+//$find[]="видел";
+    $f=$argv[1];
+    $pos = strpos($str,$f);
+    if ($pos===false){
+        echo "слова нет"."\n";
+    }
+    else{
+        if ($pos-10<0){
+            $right=substr($str,$pos);
+        };
+        $right=substr($str,$pos+strlen($f));
+        $left=substr($str,0,$pos);
+        echo  mb_substr($left,-10).','.mb_substr($right,0,10)."\n";
+
+    }
